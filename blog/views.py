34a57088bc8723c7eacf0fb148bb6c1f22475777
itachi <krajnishk@protonmail.com>
2019-10-25 16:49:10 +0530

    templating with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 
+posts = [
+    {
+        'author': 'itachi',
+        'title': 'Blog Post 1',
+        'content': 'This is the first post content',
+        'date_posted': 'Oct 24, 2019'
+    },
+    {
+        'author': 'uchiha',
+        'title': 'Blog Post 2',
+        'content': 'This is the Second post content',
+        'date_posted': 'Oct 25, 2019'
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1> Blog Home. </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About. </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
