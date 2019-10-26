c1c47dd1eb1ab2a7aeb349eeb157178f62d3eaa7
itachi <krajnishk@protonmail.com>
2019-10-26 12:59:57 +0530

    register model in admins

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
-
-
-posts = [
-    {
-        'author': 'itachi',
-        'title': 'Blog Post 1',
-        'content': 'This is the first post content',
-        'date_posted': 'Oct 24, 2019'
-    },
-    {
-        'author': 'uchiha',
-        'title': 'Blog Post 2',
-        'content': 'This is the Second post content',
-        'date_posted': 'Oct 25, 2019'
-    },
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
